58ab74e76dbcb3c5e6ab1b86e5748defff063d33
Alicia-krynne <alicemacrineadhiambo@gmail.com>
2021-05-14 22:30:07 +0300

    add first models

--- images/models.py
+++ images/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
 # Create your models here.
+
+class User(models.Model):
+  first_name = models.CharField(max_length =30)
+  last_name = models.CharField(max_length =30)
+  email = models.EmailField()
+
+  def __str__(self):
+        return self.first_name
+
+  def save_user(self):
+      self.save()
+
+
+class Images(models.Model):
+    image= models.ImageField(upload_to="gallery/")
+    title=models.CharField(max_length=200)
+    description= models.TextField(max_length=600)
+    category = models.ManyToManyField() 
+    location= models.ForeignKey()
+    user = models.ForeignKey(User,on_delete=models.CASCADE) 
+    
+   
+    
+    def __str__(self):
+        return self.title
+
+class Category(models.Model):
+    category = models.CharField(max_length =100)
+
+    def __str__(self):
+        return self.name
--- images/views.py
+++ images/views.py
@@ -1,3 +1,8 @@
+from images.models import Images
 from django.shortcuts import render
+from django.http  import HttpResponse
 
 # Create your views here.
+def my_gallery(request):
+    images = Images.my_gallery()
+    return render(request, 'all-images/my_gallery.html', {"images":images})
